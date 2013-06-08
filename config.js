6583567a3a3cc472139ae5b7c613f8e075fb637b
Sebastian Mayr <sebmaster16@gmail.com>
2013-06-08 03:45:41 +0200

    Add the ability to transcode streams

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 ﻿module.exports = {
-	mongoUrl: '',
-	mediaPath: '/media',
+	mongoUrl: '', // mongo url (no protocol)
+	mediaPath: '/media', // root path to media library
+	transcode: false, // false, 'webm'
+	transcodeParameters: ['-vb', '1M'], // -vb = variable bitrate
 
 	trakt: {
 		apiKey: 'fe70f53a3f293931235ba7d251583805'
--- server/controllers/stream.js
+++ server/controllers/stream.js
@@ -1,8 +1,30 @@
 ﻿var express = require('express');
 var config = require('../../config');
+var path = require('path');
+var spawn = require('child_process').spawn;
 
 module.exports = function () {
 	var handler = express.static(config.mediaPath);
+	if (config.transcode) {
+		handler = function (req, res) {
+			var target = path.join(config.mediaPath, decodeURI(req.path));
+			var options = ['-re', '-i', target, '-f', config.transcode, 'pipe:1'];
+			options.splice.apply(options, [5, 0].concat(config.transcodeParameters));
+
+			var ffmpeg = spawn('ffmpeg', options, { cwd: __dirname });
+
+			res.writeHead(200, {
+				'Transfer-Encoding': 'chunked',
+				'Content-Type': 'video/' + config.transcode
+			});
+
+			ffmpeg.stdout.pipe(res);
+
+			req.on('close', function () {
+				ffmpeg.kill();
+			});
+		};
+	}
 
 	return {
 		use: {
