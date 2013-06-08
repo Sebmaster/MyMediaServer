6cecfb5361deab9bf4974eef8f74ef59f597f852
Sebastian Mayr <sebmaster16@gmail.com>
2013-06-08 03:56:55 +0200

    Show thread parameter example for encoding

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 	mongoUrl: '', // mongo url (no protocol)
 	mediaPath: '/media', // root path to media library
 	transcode: false, // false, 'webm'
-	transcodeParameters: ['-vb', '1M'], // -vb = variable bitrate
+	transcodeParameters: ['-vb', '1M', '-threads', 4], // -vb = variable bitrate
 
 	trakt: {
 		apiKey: 'fe70f53a3f293931235ba7d251583805'
